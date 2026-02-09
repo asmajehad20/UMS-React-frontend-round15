99f872cc632968ef8f0be40a58dfdc8be7a5fda0
asmajehad20 <asmajehad919@gmail.com>
2026-02-09 16:11:27 +0200

    toastify added

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,12 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import App from './App.jsx'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+    <>
+      <ToastContainer />
+      <App />
+    </>
+    
 )
