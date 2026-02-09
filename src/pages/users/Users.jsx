784134af9c2a50a74baf7ccbb02f958fd099ef46
asmajehad20 <asmajehad919@gmail.com>
2026-02-09 16:04:23 +0200

    onDelete

--- src/pages/users/Users.jsx
+++ src/pages/users/Users.jsx
@@ -3,9 +3,18 @@ import Loader from "../../components/loader/Loader";
 import CardLoader from "../../components/cardLoader/CardLoader";
 import useFetch from "../../hooks/useFetch";
 import Card from "../../components/card/Card";
+import axios from "axios";
 
 export default function Users() {
   const {data, isLoader, error} = useFetch(`${import.meta.env.VITE_BURL}/users`);
+    
+  const handleDelete = async (id)=>{
+    try{
+      await axios.delete(`${import.meta.env.VITE_BURL}/users/${id}`)
+    }catch(err){
+      console.log(err);
+    }
+  }
 
     if (isLoader) {
         return (
@@ -25,7 +34,7 @@ export default function Users() {
         <div className="row g-3">
           {data.users.map((user) => {
             return (
-              <Card data={user}/>
+              <Card data={user} onDelete={handleDelete}/>
             );
           })}
         </div>
